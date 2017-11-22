53110d71090cf8941f725c231b0176e32d84e6a8
kuchu <105021033@live.asia.edu.tw>
2017-11-22 10:57:28 +0800

    1122

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: USER
+ * Date: 2017/11/22
+ * Time: 上午 10:52
+ */
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<form name="form" method="post" action="connect.php">
+    帳號：<input type="text" name="id" /> <br>
+密碼：<input type="password" name="pw" /> <br>
+<input type="submit" name="button" value="登入" />&nbsp;&nbsp;
+<a href="register.php">申請帳號</a>
+</form>
--- member.php
+++ member.php
@@ -1,14 +1,13 @@
-
-
-<?php session_start(); ?>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <?php
 /**
  * Created by PhpStorm.
  * User: USER
  * Date: 2017/11/22
- * Time: 上午 10:47
+ * Time: 上午 10:56
  */
+<?php session_start(); ?>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php
 include("mysql_connect.inc.php");
 echo '<a href="logout.php">登出</a>  <br><br>';
 //此判斷為判定觀看此頁有沒有權限
